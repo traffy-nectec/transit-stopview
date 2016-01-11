f9982fc4464f157c29921f064131c2f6c98f276e
sipp11 <sipp11@zzyzx.co>
2016-01-11 23:20:51 +0700

    Separate bus on the way to each own

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,8 @@ import CSSTransitionGroup from 'react-addons-css-transition-group'
 import ReactInterval from 'react-interval'
 import { Router, Route } from 'react-router'
 import Catalyst from 'react-catalyst'
+import cs from 'classnames'
+
 // var Navigation = ReactRouter.Navigation; // mixin
 // var History = ReactRouter.History;
 import createBrowserHistory from 'history/lib/createBrowserHistory'
@@ -13,6 +15,7 @@ import when from 'when'
 // Firebase
 import Rebase from 're-base'
 var base = Rebase.createClass('https://shining-heat-3666.firebaseio.com/');
+import cx from 'classnames';
 
 /*
   components
@@ -21,6 +24,7 @@ import NotFound from './components/NotFound'
 
 // const BUS_LOCATION_URL = '//cloud.traffy.in.th/apis/lib/apiScript/getBusLocation/getBusLocation.php?bus_line=73';
 const BUS_LOCATION_URL = '//localhost:8000/car/'
+const BUSSTOP_URL = '//localhost:8000/busstop/?limit=120&ordering=route_ref&route_id=136'
 
 /*
   App
@@ -35,14 +39,15 @@ var App = React.createClass({
       enabled: true,
       timeout: 5000,
       buses: [],
-      busAtStop: {}
+      busAtStop: {},
+      incomingBus: {},
     }
   },
 
   fetchCurrentBusLocation: function() {
     let app = this;
 
-    console.log('fetch fetchCurrentBusLocation');
+    // console.log('fetch fetchCurrentBusLocation');
     when(request({
       url: BUS_LOCATION_URL,
       method: 'GET',
@@ -51,28 +56,49 @@ var App = React.createClass({
       }
     }).then(function(response) {
       let currentBusAtStop = {};
+      let currentIncomingBus = {};
 
       response.map( (obj) => {
-        let station = obj.near_station;
-        let stationId = String(station.id_stop);
+        // let station = obj.near_station;
+        let next_station = obj.next_station;
+        let prev_station = obj.previous_station;
+        let stationId = String(next_station.id_stop);
+
         let bus = {
           // need to change into something better -- fetch from API for example
           bus_line: obj.bus_line,
           loc: [ obj.latitude, obj.longitude ],
-          plate_id: obj.plate_id
+          id: obj.bmta_id,
+          linear_ref: obj.linearref
         }
+        let relative_ref_btw_stops = (+bus.linear_ref - +prev_station.linearref)*100 /
+          (+next_station.linearref - +prev_station.linearref);
 
-        if (stationId in currentBusAtStop) {
-          currentBusAtStop[stationId].push(bus);
+        bus['ref_btw_stops'] = Math.floor(relative_ref_btw_stops);
+
+        let tenth = Math.floor(bus['ref_btw_stops']/10)
+        bus['ref_classname'] = "d" + ( (tenth > 1 && tenth < 9) ? tenth : '' ) + "0";
+
+        if (bus.ref_btw_stops > 85) {
+          if (stationId in currentBusAtStop) {
+            currentBusAtStop[stationId].push(bus);
+          } else {
+            currentBusAtStop[stationId] = [ bus ];
+          }
         } else {
-          currentBusAtStop[stationId] = [ bus ]
+          if (stationId in currentIncomingBus) {
+            currentIncomingBus[stationId].push(bus);
+          } else {
+            currentIncomingBus[stationId] = [ bus ];
+          }
         }
-
+        // console.log("at: #"+currentBusAtStop.length + " | in: #" + currentIncomingBus.length);
       } );
 
       app.setState({
         buses: response,
-        busAtStop: currentBusAtStop
+        busAtStop: currentBusAtStop,
+        incomingBus: currentIncomingBus,
       });
 
       return true;
@@ -86,9 +112,24 @@ var App = React.createClass({
   },
 
   componentDidMount: function() {
-    this.setState({
+    /*this.setState({
       busStops: require('./all-bus-stops')
-    })
+    })*/
+    let app = this;
+
+    when(request({
+      url: BUSSTOP_URL,
+      method: 'GET',
+      headers: {
+        // 'Authorization': 'Bearer ',
+      }
+    }).then(function(response) {
+      app.setState({
+        busStops: response.results
+      })
+
+    }))
+
   },
   
   render: function() {
@@ -100,11 +141,13 @@ var App = React.createClass({
         <div className="content">
           <ul className="inline-list">
             <li><label><input type="checkbox" ref="enableUpdate" checked={this.state.enabled}
-          onClick={this.toggleEnableUpdate.bind(null, this)} /> update?</label></li>
+          onChange={this.toggleEnableUpdate.bind(null, this)} /> update?</label></li>
             <li>Bus Total: {this.state.buses.length}</li>
           </ul>
         </div>
-        <BusStopList busStops={this.state.busStops} busAtStop={this.state.busAtStop} />
+        <BusStopList busStops={this.state.busStops}
+          busAtStop={this.state.busAtStop}
+          incomingBus = {this.state.incomingBus} />
       </div>
     )
   }
@@ -121,7 +164,8 @@ var BusStopList = React.createClass({
   renderBusStop: function(key) {
     return (
       <BusStop key={key} index={key} details={this.props.busStops[key]}
-        busAtStop={this.props.busAtStop} />
+        busAtStop={this.props.busAtStop}
+        incomingBus={this.props.incomingBus} />
     )
   },
   render: function() {
@@ -135,10 +179,29 @@ var BusStopList = React.createClass({
   },
   propTypes: {
     busAtStop: React.PropTypes.object.isRequired,
+    incomingBus: React.PropTypes.object.isRequired,
     busStops: React.PropTypes.array.isRequired
   }
 });
 
+
+
+var BusItemList = React.createClass({
+
+  render: function() {
+    let items = this.props.items;
+    if (items.length == 0) {
+      return <i></i>
+    } else {
+      return (
+        <li><b>{this.props.title}</b>:
+          {items.map( (i) => i.id + " (" + i['ref_btw_stops'] + ") " )}
+        </li>
+      )
+    }
+  }
+
+})
 /*
   BusStop
   <BusStop/>
@@ -150,24 +213,41 @@ var BusStop = React.createClass({
     let detail = this.props.details;
     // check if there is incoming bus
     let incoming = [];
-    if (String(detail.id) in this.props.busAtStop) {
-      incoming = this.props.busAtStop[String(detail.id)];
+    let atStop = [];
+    if (String(detail.stop_id) in this.props.incomingBus) {
+      incoming = this.props.incomingBus[String(detail.stop_id)];
+      // console.log('incoming');
+      // console.log(incoming);
     }
+    if (String(detail.stop_id) in this.props.busAtStop &&
+          this.props.busAtStop[String(detail.stop_id)] != undefined) {
+      atStop = this.props.busAtStop[String(detail.stop_id)];
+      // console.log('at stop');
+      // console.log(atStop);
+    }
+    let incomingClassDict = {
+      'cbp_tmicon': true, 'ontheway': true
+    };
+    // console.log();
+    let incomingClassName = cx(incomingClassDict);
+    // console.log(incoming);
+    // console.log(atStop);
     // check if there is a bus at stop
     return (
       <li>
         <div className="block">
           <time className="cbp_tmtime">
-            <span></span>
+            <span>{ atStop.length ? atStop.length : '' }</span>
             <span></span></time>
           <div className="cbp_tmicon">
+            { atStop.length ? this.renderBusIcon() : null }
           </div>
           <div className="cbp_tmlabel">
-            <h4>{detail.name} <small>#{detail.id}</small></h4>
-            <p>
-              { incoming.length ? <b>Incoming bus:</b> : '' }
-              {incoming.map( (bus) =>  bus.plate_id + "," )}
-            </p>
+            <h4>{detail.name} <small>#{detail.stop_id}</small></h4>
+            <ul>
+              <BusItemList title="At stop" items={atStop} />
+              <BusItemList title="Incoming" items={incoming} />
+            </ul>
           </div>
         </div>
 
@@ -175,7 +255,16 @@ var BusStop = React.createClass({
           <time className="cbp_tmtime">
             <span>{ incoming.length ? incoming.length : '' }</span>
             <span></span></time>
-          <div className="cbp_tmicon ontheway">{ incoming.length ? this.renderBusIcon() : null }</div>
+          {incoming.map( (ele, i) => {
+            let xClsDict = [ele.ref_classname, 'cbp_tmicon', 'ontheway'];
+            let xClsName = cx(xClsDict);
+            let key = "car-icon-" + i;
+            return (
+              <div className={xClsName} key={key}>
+                { incoming.length ? this.renderBigBusIcon() : null }
+              </div>
+            )
+          })}
           <div className="cbp_tmblank"></div>
         </div>
       </li>
@@ -184,8 +273,12 @@ var BusStop = React.createClass({
   renderBusIcon: function() {
     return <i className="fa fa-bus"></i>
   },
+  renderBigBusIcon: function() {
+    return <i className="fa fa-bus fa-2x"></i>
+  },
   propTypes: {
     busAtStop: React.PropTypes.object.isRequired,
+    incomingBus: React.PropTypes.object.isRequired,
     details: React.PropTypes.object.isRequired
   }
 
