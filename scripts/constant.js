e43cf3bdad445ac6b80a3fc7b98c085c7aed20e9
sipp11 <sipp11@zzyzx.co>
2016-02-24 15:43:41 +0700

    Change URL endpoint

--- scripts/constant.js
+++ scripts/constant.js
@@ -4,7 +4,7 @@ const BASE_API_URL = 'http://api.traffy.xyz/';
 let constants = {
   GA_TRACKING_ID : 'UA-72505920-1',
   BUS_LOCATION_URL : BASE_API_URL + 'car/',
-  BUSSTOP_URL : BASE_API_URL + 'busstop/?limit=120&ordering=route_ref',
+  BUSSTOP_URL : BASE_API_URL + 'stop/?limit=120&ordering=route_ref',
 }
 
 export default constants;
